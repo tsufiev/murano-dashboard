711a6fec2b19977ad58db0d221086f7e2074d769
Timur Sufiev <tsufiev@mirantis.com>
2015-02-13 18:32:50 -0800

    [WIP] Support actions with TableWidget
    Change-Id: Iac45c971fac311182cda719a9fa831dec9474cb7

--- muranodashboard/environments/tabs.py
+++ muranodashboard/environments/tabs.py
@@ -111,7 +111,7 @@ class OverviewTab(tabs.Tab):
                     detail_info['Stack'] = stack
 
         if hasattr(service_data,
-                   'instances') and service_data['instance'] is not None:
+                   'instances') and service_data['instances'] is not None:
                 instances_for_template = []
                 stacks_for_template = []
                 for instance in service_data['instances']:
@@ -125,22 +125,6 @@ class OverviewTab(tabs.Tab):
                     detail_info['Stacks'] = stacks_for_template
 
         return {'service': detail_info}
-
-
-class AppActionsTab(tabs.Tab):
-    name = _('Actions')
-    slug = '_actions'
-    template_name = 'services/_actions.html'
-
-    def get_context_data(self, request):
-        data = self.tab_group.kwargs['service']
-        return {'actions': api.extract_actions_list(data),
-                'service_id': self.tab_group.kwargs['service_id'],
-                'environment_id': self.tab_group.kwargs['environment_id']}
-
-    def allowed(self, request):
-        environment_id = self.tab_group.kwargs['environment_id']
-        return api.action_allowed(request, environment_id)
 
 
 class ServiceLogsTab(tabs.Tab):
@@ -205,7 +189,7 @@ class EnvironmentTopologyTab(tabs.Tab):
 
 class EnvironmentServicesTab(tabs.TableTab):
     name = _("Components")
-    slug = "serviceslist"
+    slug = "services"
     table_classes = (tables.ServicesTable,)
     template_name = "services/_service_list.html"
     preload = False
@@ -266,7 +250,7 @@ class EnvironmentDetailsTabs(tabs.TabGroup):
 
 class ServicesTabs(tabs.TabGroup):
     slug = "services_details"
-    tabs = (OverviewTab, AppActionsTab, ServiceLogsTab)
+    tabs = (OverviewTab, ServiceLogsTab)
 
 
 class DeploymentDetailsTabs(tabs.TabGroup):
